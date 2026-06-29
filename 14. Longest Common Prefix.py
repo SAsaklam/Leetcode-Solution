d02d0854d5df2b374fb8310e67a5f6e4d1ae68b9
SAsaklam <saklamsakib325@gmail.com>
2026-06-29 17:17:20 +0600

    Implement longest common prefix algorithm

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        strs.sort()
+
+        first = strs[0]
+        last = strs[-1]
+
+        result = []
+
+        
+        for i in range(len(first)):
+            if i >= len(last) or first[i] != last[i]:
+                break
+            result.append(first[i])
+
+        return "".join(result)
