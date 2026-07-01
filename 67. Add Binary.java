ffb0aaf219ada664ac4b2b3847db18273a7b4f1d
SAsaklam <saklamsakib325@gmail.com>
2026-07-01 18:58:18 +0600

    Implement binary addition in Add Binary.java

---
+++ 67. Add Binary.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String addBinary(String a, String b) {
+
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+
+            int c = i >= 0 ? a.charAt(i) - '0' : 0;
+            int d = j >= 0 ? b.charAt(j) - '0' : 0;
+
+            int sum = c + d + carry;
+
+            sb.append(sum % 2);  
+            carry = sum / 2;     
+
+            i--;
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
+
